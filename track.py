62470abd4673ae0d9c702c29201b14ec5bb9f5fa
Victor <victordpnd@tutamail.com>
2026-07-10 20:02:26 +0300

    Create track.py

---
+++ track.py
@@ -0,0 +1,7 @@
+class Track:
+
+    def __init__(self, title, artist, duration, rating):
+        self.title = title
+        self.artist = artist
+        self.duration = duration
+        self.rating = rating
