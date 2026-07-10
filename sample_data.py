29b5450a14ab6c1cd55e9e91442ff6464c758555
Victor <victordpnd@tutamail.com>
2026-07-10 20:03:37 +0300

    Add sample_data.py with track list

---
+++ sample_data.py
@@ -0,0 +1,8 @@
+from track import Track
+
+tracks = [
+    Track("Dreams", "Fleetwood Mac", 257, 5),
+    Track("Imagine", "John Lennon", 183, 5),
+    Track("Africa", "Toto", 295, 4),
+    Track("Take On Me", "a-ha", 225, 5),
+]
